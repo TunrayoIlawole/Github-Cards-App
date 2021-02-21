f2d3de0f70cbff5dbee2ebf61d29abef1b8cc1ca
Nifemi <deyeminifemi@gmail.com>
2021-02-22 00:34:21 +0100

    LOADER ADDED AHHHHHH

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Card from './Card';
 import {connect} from 'react-redux';
+import RingLoader from 'react-spinners/RingLoader';
+import {useMemo, useState} from 'react';
 
-const CardList = (props) => {
-    const testData = props.profile.profiles;
-    const cards = testData.map(profile => {
-        return <Card {...profile} />
-    })
+const CardList = ({ profile }) => {
+    const { profiles, isLoading } = profile;
+
+    const cards = useMemo(() => {
+        return profiles.map(profile => {
+            return <Card {...profile} />
+        })
+    }, [profiles])
+
     return (
         <div>
+            {isLoading && <RingLoader size={30} color='violet'/>}
             {cards}
         </div>
     )
